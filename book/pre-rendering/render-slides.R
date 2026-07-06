e1a5607d862eaad333b6019851742d8406d9df70
Mooij-van Malsen <j.g.demooij-vanmalsen@uu.nl>
2026-07-06 14:41:00 +0200

    rerendering slides

---
+++ book/pre-rendering/render-slides.R
@@ -0,0 +1,8 @@
+library(quarto)
+print("Let's render the Quickstart slides first")
+quarto_render("book/slides/slides_quick.qmd") #renders slides to book/slides
+
+print("Done rendering the slides! Now onto the book itself...")
+
+
+
